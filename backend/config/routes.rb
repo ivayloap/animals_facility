8e44cdbe60e2a5262c4c96fd3457a18e583b5fba
Ivaylo Petrov <ivaylo.ap@gmail.com>
2023-07-03 11:13:26 +0300

    Add session controller

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'login', to: 'sessions#new'
+  delete 'logout', to: 'sessions#destroy'
 end
